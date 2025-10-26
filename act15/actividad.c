11cd3298d3f86a4e9a8829335dd5ad89e47b8ba0
diegoa-mg <ddiegoa.mg@gmail.com>
2025-10-25 19:17:50 -0600

    Avances act15

--- act15/actividad.c
+++ act15/actividad.c
@@ -18,33 +18,45 @@ int main(){
         printf("11. Salir\n");
         printf("Seleccione una opcion: ");
         scanf("%d", &opc);
-        printf("\n");
 
         switch(opc){
             case 1:{
-                int num;
+                printf("\nEjercicio 1: Numeros primos del 1 al 500\n");
+                int num, ultimoprimo = 0;
 
-                for(num = 1; num <= 500; num++){
-                    if(num / 2 == 0){
-                        if(num == 2){
-                            printf("%d, ", num);
+                for(num = 2; num <= 500; num++){
+                    int esPrimo = 1; // Suponemos que el numero es primo
+
+                    // num = 2. Al comenzar el bucle, en mi condicion como 2 no es <= a 1, se salta el bucle y pasa los if
+                    for(int i = 2; i <= num / 2; i++){ // El divisor mas grande que puede tener un numero es su mitad (ademas de si mismo)
+                        if(num % i == 0){
+                            esPrimo = 0; // No es primo
+                            break;
                         }
                     }
-                    else{
-                        printf("%d, ", num);
-                    }                         
+
+                    if(esPrimo){ // esPrimo == 1. DATO: esPrimo == 0 equivale a !esPrimo
+                        if(ultimoprimo != 0){
+                            printf("%d, ", ultimoprimo);
+                        }
+                        ultimoprimo = num;
+                    }        
                 }
-        
+                printf("%d\n", ultimoprimo);
+                // Se usa una tecnica del buffer para no imprimir la coma despues del ultimo numero
+                // Se guarda el ultimo numero primo y se imprime despues del bucle
+                // Dentro del bucle, se imprime el numero primo anterior antes de actualizarlo
                 break;
             }
 
             case 2:{
+                printf("\nEjercicio 2: Obtener perimetros de triangulos\n");
                 int lado, perimetro;
                 do
                 {
                     perimetro = 0;
                     for(int i = 1; i <= 3; i++){
-                        printf("Ingrese el lado %d del triangulo (0 para salir): ", i);
+                        printf("\nIngrese el lado %d del triangulo (0 para salir): ", i);
                         scanf("%d", &lado);
                         if(lado == 0){
                             break;
@@ -58,19 +70,64 @@ int main(){
                 
                 break;
             }
-            case 3:
-                // Ejercicio 3 code here
-                break;
+            case 3:{
+                printf("\nEjercicio 3: Obtener perimetros de triangulos\n");
+                int lado, perimetro;
+                char elegir;
 
-            case 4:
-                // Ejercicio 4 code here
+                do
+                {
+                    perimetro = 0;
+                    for(int i = 1; i <= 3; i++){
+                        printf("\nIngrese el lado %d del triangulo: ", i);
+                        scanf("%d", &lado);
+                        perimetro = perimetro + lado;
+                    }    
+                    printf("\nEl perimetro del triangulo es: %d\n", perimetro);
+                    printf("Desea calcular otro perimetro? (s/n): ");
+                    scanf(" %c", &elegir);
+                } while(elegir == 's' || elegir == 'S');
+                
                 break;
+            }
+            case 4:{
+                printf("\nEjercicio 4: Calcular combinacion de productos\n");
+                int total = 87, prod1 = 5, prod2 = 7, prod3 = 19;
+                int x, y, z;
 
+                x = total / prod1; // Maximo de prod1
+                y = total / prod2; // Maximo de prod2
+                z = total / prod3; // Maximo de prod3
+
+                
+
+
+
+                break;
+            }
             case 5:
-                // Ejercicio 5 code here
+                printf("\nEjercicio 5: Detectar todos los divisores de un numero\n");
+                int num, ultimonumero = 0, esDivisor = 1;
+
+                printf("Ingrese un numero entero positivo: ");
+                scanf("%d", &num);
+
+                for(int i = 1; i <= num / 2; i++){
+                    if(num % i != 0){
+                        esDivisor = 0; // No es divisor
+                    }
+                    else{
+                        if(ultimonumero != 0){
+                            printf("%d, ", ultimonumero);
+                        }
+                        ultimonumero = i;
+                    }
+                }
+                printf("%d\n", ultimonumero);
                 break;
 
             case 6: {
+                printf("\nEjercicio 6: Simplificar fracciones\n");
                 int num1, num2, sim1, sim2, MCD = 1, menor;
 
                 printf("Ingresa una fraccion (Ej: 15/18):\n");
@@ -123,6 +180,7 @@ int main(){
             }
 
             case 7: {
+                printf("\nEjercicio 7: Calcular distancia recorrida\n");
                 float distancia, velocidad, tiempo;
                 char elegir;
 
@@ -144,6 +202,7 @@ int main(){
             }
 
             case 8: {
+                printf("\nEjercicio 8: Convertir de pulgadas a centimetros\n");
                 float cm, inches;
 
                 printf("Ingrese la medida en pulgadas: ");
@@ -161,6 +220,7 @@ int main(){
                     break;
             }
             case 9: {
+                printf("\nEjercicio 9: Mostrar nombre y salario de 120 empleados\n");
                 char nombre[50];
                 float salario;
 
@@ -182,6 +242,7 @@ int main(){
             }
 
             case 10: {
+                printf("\nEjercicio 10: Mostrar nombre y promedio de 3 examenes, de 25 alumnos\n");
                 float calif, promedio;
                 char nombre[50];
 
@@ -204,11 +265,11 @@ int main(){
                 break;
             }
             case 11:
-                printf("Saliendo del programa...\n");
+                printf("\nSaliendo del programa...\n");
                 break;
 
             default:
-                printf("Opcion no valida. Por favor, intente de nuevo.\n");
+                printf("\nOpcion no valida. Por favor, intente de nuevo.\n");
                 break;
         }
     }while(opc != 11);
